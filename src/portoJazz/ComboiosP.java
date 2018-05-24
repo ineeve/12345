98a37078601e276ee8f4b89c443d01ce5c092aeb
ineeve <renatoineeve@gmail.com>
2018-05-24 20:45:03 +0100

    added class origem

--- src/portoJazz/ComboiosP.java
+++ src/portoJazz/ComboiosP.java
@@ -3,25 +3,13 @@ package portoJazz;
 
 public class ComboiosP extends Bilhete{
 
-	private String origem;
-	private float precoViagem;
+	private Origem origem;
 	
 	public ComboiosP(String origem)
 	{
 		super();
 		super.setPreco(5.00f);
-		super.setTipo("Bilhete CP");
-		switch (origem)
-		{
-		case "Porto": 	this.precoViagem = 1.50f;
-						break;
-		case "Lisboa":  this.precoViagem = 6.30f;
-						break;		
-		case "Algarve":	this.precoViagem = 15.60f;
-						break;
-		case "Braga":	this.precoViagem = 3.60f;
-		break;		
-		}		
+		super.setTipo("Bilhete CP");	
 	}
 	
 	
@@ -29,27 +17,23 @@ public class ComboiosP extends Bilhete{
 	
 	public String getOrigem() 
 	{
-		return origem;
+		return origem.getLocal();
 	}
 	public float getPrecoViagem() 
 	{
-		return precoViagem;
+		return origem.getPrecoViagem();
 	}
 //SETTERS
-	public void setOrigem(String origem) 
+	public void setOrigem(Origem origem) 
 	{
 		this.origem = origem;
-	}
-	public void setPrecoViagem(float precoViagem) 
-	{
-		this.precoViagem = precoViagem;
 	}
 //Outros Metodos
 
 	public void imprimirPreco()
 	{
-		System.out.println("O preço do bilhete do Festivel será: "+getPreco());	
-		System.out.println("O preço da viagem será: "+getPrecoViagem());
+		System.out.println("O preï¿½o do bilhete do Festivel serï¿½: "+getPreco());	
+		System.out.println("O preï¿½o da viagem serï¿½: "+getPrecoViagem());
 		System.out.println("Total: "+(this.getPrecoViagem()+super.getPreco()));
 	}
 	
---
+++ src/portoJazz/Porto.java
@@ -0,0 +1,9 @@
+package portoJazz;
+
+public class Porto extends Origem {
+
+    public Porto(){
+        this.setLocal("Porto");
+        this.setPrecoViagem(1.50f);
+    }
+}
